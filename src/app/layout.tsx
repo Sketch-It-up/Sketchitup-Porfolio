3fe17e508c8b53705d97b101a16199369054cde8
DBjay01 <dhananjayvbile10@gmail.com>
2025-03-25 18:12:58 +0530

    Microsoft Clarity

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,12 +2,13 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import clsx from "clsx";
+import Head from "next/head";
 
 const dmSans = DM_Sans({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Sketch It Up",
-  description: "a Software consulting company",
+  description: "A Software consulting company",
 };
 
 export default function RootLayout({
@@ -17,6 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" className="relative">
+      <Head>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+              })(window, document, "clarity", "script", "qtqsiwgkh1");
+            `,
+          }}
+        />
+      </Head>
       <body className={clsx(dmSans.className, "antialiased bg-[#EAEEFE]")}>
         {children}
       </body>
